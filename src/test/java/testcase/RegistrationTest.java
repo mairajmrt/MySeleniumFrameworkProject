d997e1429c32db8caa9199ec7f4a1b0ef47f3cf0
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-03-09 17:10:30 +0530

    comitted by mairaj

---
+++ src/test/java/testcase/RegistrationTest.java
@@ -0,0 +1,15 @@
+package testcase;
+
+public class RegistrationTest {
+
+	public static void main(String[] args) {
+		System.out.println("Login Test RUnning");
+		String food = "apple";
+		if (food == "apple") {
+			System.out.println("We have eaten food");
+		} else
+			System.out.println("You are hungry");
+
+	}
+
+}
